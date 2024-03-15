5fc23410830550b0d8dd064163ea1af95974e559
elev <ysh1059@163.com>
2024-03-15 15:53:20 +0800

    add py

---
+++ leetcode/016.py
@@ -0,0 +1,27 @@
+from math import inf
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        n = len(nums)
+        ans = inf
+        for i, v in enumerate(nums):
+            j, k = i + 1, n - 1
+            while j < k:
+                t = v + nums[j] + nums[k]
+                if t == target:
+                    return t
+                if abs(t - target) < abs(ans - target):
+                    ans = t
+                if t > target:
+                    k -= 1
+                else:
+                    j += 1
+        return ans
+    
+nums = [-1,2,1,-4]
+target = 1
+
+print(Solution().threeSumClosest(nums,target))

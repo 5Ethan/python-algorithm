7bf29a044563241a9f5a710a60b8c6c0cbfcc75d
elev <ysh1059@163.com>
2024-03-15 15:47:49 +0800

    add py

--- leetcode/015.py
+++ leetcode/015.py
@@ -3,6 +3,7 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # 题目不要求我们按照顺序返回三元组，因此我们不妨先对数组进行排序，这样就可以方便地跳过重复的元素。
         nums.sort()
         n = len(nums)
         ans = []
@@ -13,6 +14,11 @@ class Solution:
                 continue
             j, k = i + 1, n - 1
             while j < k:
+                """
+                如果x<0，则说明nums[j]太小，我们需要将j右移一位
+                如果x>0，则说明nums[k]太大，我们需要将k左移一位
+                如果找到合法的三元组，则将其加入答案，并将j右移一位，k左移一位
+                """
                 x = nums[i] + nums[j] + nums[k]
                 if x < 0:
                     j += 1
